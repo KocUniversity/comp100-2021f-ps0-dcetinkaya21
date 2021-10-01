cbd51285d4ab9abfc830a25d441338b8a0c2ab26
dcetinkaya21 <91727315+dcetinkaya21@users.noreply.github.com>
2021-10-01 10:01:09 +0000

    done

---
+++ main.py
@@ -0,0 +1,8 @@
+x= int(input('Please enter number x'))
+y= int(input('Please enter number y'))
+exponential= x**y
+print('The number x, raised to the power of y is', exponential )
+import numpy
+print( numpy.log2(x))
+print("log(x) =", numpy.log2(x))
+print('77878')
